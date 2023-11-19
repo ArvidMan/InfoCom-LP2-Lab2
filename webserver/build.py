3141b68e52c69357ecfd4ce5825f606c715214f9
Arvid <ar3126ma-s@student.lu.se>
2023-11-19 18:02:59 +0100

    lagt till funktioner, funkar inte än

--- webserver/build.py
+++ webserver/build.py
@@ -9,9 +9,9 @@ CORS(app, supports_credentials=True)
 app.secret_key = 'dljsaklqk24e21cjn!Ew@@dsa5'
 socket = SocketIO(app, cors_allowed_origins="*")
 
-# change this so rhat you can connect to your redis server
+# change this so that you can connect to your redis server
 # ===============================================
-redis_server = redis.Redis("YOUR_SERVER")
+redis_server = redis.Redis(host = '0.0.0.0', port=7777)
 # ===============================================
 
 # Translate OSM coordinate (longitude, latitude) to SVG coordinates (x,y).
@@ -42,8 +42,8 @@ def get_location():
     while True:
         #get your longitude and latitude from the Redis server
         # ====================================================
-        longitude = None
-        latitude = None
+        longitude = redis_server.get('longitude').decode('utf-8')
+        latitude  = redis_server.get('latitude').decode('utf-8')
         # ====================================================
         x_svg, y_svg = translate((longitude, latitude))
         emit('get_location', (x_svg, y_svg))
